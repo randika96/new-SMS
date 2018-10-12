8ce073f7b2c12d63ae6ce4edf81827c87654bcfb
rpjayasekara <rpjayasekara.16@cse.mrt.ac.lk>
2018-10-12 20:16:32 +0530

    add  mongolab

--- routes/index.js
+++ routes/index.js
@@ -31,7 +31,7 @@ router.get("/dashbord", isLoggedIn, function (req,res) {
                             // notice:notice.notice
                         }
                         console.log(newNotice);
-                        res.render("admin/index",{notice:notice});
+                        res.render("admin/index");
                         console.log(admin);
                     }
                 })
